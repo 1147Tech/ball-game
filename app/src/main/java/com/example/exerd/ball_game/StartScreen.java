7c9f5b5dce8d57a1125d8f843ff8a26a5490a422
exerdra <exerdra@gmail.com>
2018-08-30 21:59:36 -0500

    added button functions

--- app/src/main/java/com/example/exerd/ball_game/StartScreen.java
+++ app/src/main/java/com/example/exerd/ball_game/StartScreen.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
-
+import android.widget.Button;
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -102,11 +102,26 @@ public class StartScreen extends AppCompatActivity {
             }
         });
 
+        Button button = findViewById(R.id.quit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ASLevelActivity.class);
+//                startActivity(intent);
+                finishAndRemoveTask();
+            }
+        });
+
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.play_button).setOnTouchListener(mDelayHideTouchListener);
     }
+
+
+
+
+
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
